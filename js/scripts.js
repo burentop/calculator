84118a000f8157f541cc1b5b0eed7d1ba16018e4
burentoperii@gmail.com <burentoperii@gmail.com>
2018-08-11 15:33:09 -0500

    complete bmi and ctof functions

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,18 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
+var bmi = function(weight, height) {
+  return (weight / (height * height)) * 703;
+};
+
+var ctof = function(c) {
+  return c * (9/5) + 32;
+};
+
+//var number1 = parseInt(prompt("What is your weight in lbs:"));
+// var number2 = parseInt(prompt("What is your height in inches:"));
+// var result = bmi(number1, number2);
+var cel = parseInt(prompt("What is the temp in C:"));
+var result = ctof(cel);
 
 alert(result);
